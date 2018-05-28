0d84cfe6ca833a012eb104c0a891463ff3d28611
maybeluwang <maybelu.wang@gmail.com>
2018-05-28 10:00:16 +0800

    Pad zeros at the end of MFCC frames to align

--- dataReader.py
+++ dataReader.py
@@ -1,7 +1,12 @@
-import numpy
+# This file is used to create a new dataset of Librispeech with tensorflow's API 
+# to make the tensorflow training process and I/O simultaneously.
+
+# We pad zeros in the end the MFCC frames to align for the use of using RNN structure.
+
+import numpy as np
 import tensorflow as tf
 
-lines = 600000
+lines = 300000
 fo = open("Librispeech/cmvned_feats.ark")
 fans = open("Librispeech/all_prons")
 writer = tf.python_io.TFRecordWriter('%s.tfrecord' %'test')
@@ -28,15 +33,19 @@ for j in range(lines):
         sentenceNum = line[0].split('-')[2]
         fo.readline()
 
+    MFCC_pad0 = np.zeros(400*39)  # MFCC frame is at most 400 for a single word
+    
     each_word = []
     for i in range(length):
         line = fo.readline()
         bb_list = filter(lambda k: k not in ['\n',']',']\n'],line.split(' '))
-        each_word.append( [float(i) for i in bb_list if i])
-    each_word = numpy.array(each_word)
-    
-    features['matrix'] = tf.train.Feature(float_list = tf.train.FloatList(value=each_word.reshape(-1)))
-    features['matrix_shape'] = tf.train.Feature(int64_list = tf.train.Int64List(value=each_word.shape))
+        bb_list_array = [float(i) for i in bb_list if i]
+        each_word.append( bb_list_array )
+    each_word = np.array(each_word)
+
+    np.put(MFCC_pad0, np.arange(length), each_word)
+    features['matrix'] = tf.train.Feature(float_list = tf.train.FloatList(value=MFCC_pad0))
+    features['matrix_shape'] = tf.train.Feature(int64_list = tf.train.Int64List(value=(400, 39)))
 
     tf_features = tf.train.Features(feature= features)
     tf_example = tf.train.Example(features = tf_features)
